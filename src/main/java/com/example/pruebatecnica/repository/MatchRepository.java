d0adcdbe8ea772fb1b7905e6ef3e321471376cb3
luislftv <81823018+luislftv@users.noreply.github.com>
2025-01-29 06:22:33 -0500

    Actualizar MatchRepository.java

--- src/main/java/com/example/pruebatecnica/repository/MatchRepository.java
+++ src/main/java/com/example/pruebatecnica/repository/MatchRepository.java
@@ -11,12 +11,12 @@ public interface MatchRepository extends MongoRepository<Match, String> {
     List<Match> findByTournamentId(String tournamentId);
 
     @Aggregation(pipeline =      {
-            "{ '$match': { 'tournamentId': '6798eab9fb07c04a10beb2f1' } }",
+            "{ '$match': { 'tournamentId': ?0 } }",
             "{ '$facet': { 'homeStats': [{ '$group': { '_id': '$homeTeam.teamId', 'matchesPlayed': { '$sum': 1 }, 'goalsScored': { '$sum': '$homeTeam.score' } } }], 'awayStats': [{ '$group': { '_id': '$awayTeam.teamId', 'matchesPlayed': { '$sum': 1 }, 'goalsScored': { '$sum': '$awayTeam.score' } } }] } }",
             "{ '$project': { 'teams': { '$concatArrays': ['$homeStats', '$awayStats'] } } }",
             "{ '$unwind': '$teams' }",
             "{ '$group': { '_id': '$teams._id', 'matchesPlayed': { '$sum': '$teams.matchesPlayed' }, 'goalsScored': { '$sum': '$teams.goalsScored' } } }",
-            "{ '$lookup': { 'from': 'tournaments', 'let': { 'teamId': '$_id' }, 'pipeline': [{ '$addFields': { 'tournamentIdStr': { '$toString': '$_id' } } }, { '$match': { '$expr': { '$eq': ['$tournamentIdStr', '6798eab9fb07c04a10beb2f1'] } } }, { '$unwind': '$teams' }, { '$match': { '$expr': { '$eq': ['$teams._id', '$$teamId'] } } }, { '$project': { 'teamName': '$teams.name' } }], 'as': 'teamData' } }",
+            "{ '$lookup': { 'from': 'tournaments', 'let': { 'teamId': '$_id' }, 'pipeline': [{ '$addFields': { 'tournamentIdStr': { '$toString': '$_id' } } }, { '$match': { '$expr': { '$eq': ['$tournamentIdStr', ?0] } } }, { '$unwind': '$teams' }, { '$match': { '$expr': { '$eq': ['$teams._id', '$$teamId'] } } }, { '$project': { 'teamName': '$teams.name' } }], 'as': 'teamData' } }",
             "{ '$unwind': { 'path': '$teamData', 'preserveNullAndEmptyArrays': true } }",
             "{ '$project': { 'teamId': '$_id', 'teamName': { '$ifNull': ['$teamData.teamName', 'Unknown'] }, 'matchesPlayed': 1, 'goalsScored': 1 } }"})
     List<TeamStatsProjection> getAllTeamsStats(String tournamentId);
